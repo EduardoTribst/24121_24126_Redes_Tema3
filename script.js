dbd5d8082021a20ca54b87a4674a46d2b6a5a695
Eduardo Tribst <eduardo.tribst@gmail.com>
2025-05-05 22:05:50 -0300

    arrumei umas coisas e coloquei outras

---
+++ script.js
@@ -0,0 +1,19 @@
+
+function mudar(novaPagina){
+    
+    
+    if (novaPagina == "Autores"){
+        location.replace("https://eduardotribst.github.io/24121_24126_Redes_Tema3/autores.html")
+    }
+    else if (novaPagina == "OProjeto"){
+        location.replace("https://eduardotribst.github.io/24121_24126_Redes_Tema3/oprojeto.html")
+    }
+    else if (novaPagina == "Redes"){
+        location.replace("https://eduardotribst.github.io/24121_24126_Redes_Tema3/redes.html")
+    }
+    else if (novaPagina == "Referencias"){
+        location.replace("https://eduardotribst.github.io/24121_24126_Redes_Tema3/referencias.html")
+    }
+    
+    
+}
