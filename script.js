6191b6e2310a0068824baae2fb1f586c13c785c5
EduardoTribst <eduardo.tribst@gmail.com>
2025-05-07 09:29:13 -0300

    index0

--- script.js
+++ script.js
@@ -3,19 +3,13 @@ function mudar(novaPagina){
     
     
     if (novaPagina == "Autores"){
-        location.replace("https://eduardotribst.github.io/24121_24126_Redes_Tema3/autores.html")
-    }
-    else if (novaPagina == "OProjeto"){
-        location.replace("https://eduardotribst.github.io/24121_24126_Redes_Tema3/oprojeto.html")
-    }
-    else if (novaPagina == "Redes"){
-        location.replace("https://eduardotribst.github.io/24121_24126_Redes_Tema3/redes.html")
+        location.replace("http://127.0.0.1:5500/autores.html")
     }
     else if (novaPagina == "Referencias"){
-        location.replace("https://eduardotribst.github.io/24121_24126_Redes_Tema3/referencias.html")
+        location.replace("http://127.0.0.1:5500/referencias.html")
     }
 }
 
 function voltar(){
-    location.replace("https://eduardotribst.github.io/24121_24126_Redes_Tema3/index.html")
+    location.replace("http://127.0.0.1:5500/index.html")
 }
