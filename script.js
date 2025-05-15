71d9228109a3a5d0e3530fd1f55c38ec5aa99541
EduardoTribst <eduardo.tribst@gmail.com>
2025-05-15 13:30:21 -0300

    menu quase pronto

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 
+const toggleBtn = document.getElementById('menu-toggle');
+const closeBtn = document.getElementById('close-menu');
+const sidebar = document.getElementById('sidebar');
+const overlay = document.getElementById('overlay');
+
+function openMenu() {
+    sidebar.classList.add('show');
+    overlay.classList.add('show');
+  }
+  
+  function closeMenu() {
+    sidebar.classList.remove('show');
+    overlay.classList.remove('show');
+  }
+  
+  toggleBtn.addEventListener('click', openMenu);
+  closeBtn.addEventListener('click', closeMenu);
+  overlay.addEventListener('click', closeMenu);
+
 function mudar(novaPagina){
-    
     
     if (novaPagina == "Autores"){
         location.replace("http://127.0.0.1:5500/autores.html")
@@ -13,3 +31,4 @@ function mudar(novaPagina){
 function voltar(){
     location.replace("http://127.0.0.1:5500/index.html")
 }
+
